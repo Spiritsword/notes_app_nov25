6a2f32f39d90a9c4e130d1b927eb72133ac7fa69
spiritsword <18164236+Spiritsword@users.noreply.github.com>
2025-11-01 17:30:24 +0000

    Sketching structure of server.js

---
+++ code/server.js
@@ -0,0 +1,11 @@
+//Initialisation
+
+//Instructions for serving index.html, styles.css and script.js
+
+//Instructions for responding to CRUD requests from client
+
+//Create a note
+//Read all notes
+//(Read a particular note)
+//Update a particular note
+//Delete a particular note
