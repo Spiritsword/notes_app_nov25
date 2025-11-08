33254e39658feabe5a37869406e7e04230e5a1aa
spiritsword <18164236+Spiritsword@users.noreply.github.com>
2025-11-08 17:44:13 +0000

    Resetting the file contents to separate concerns

--- server.js
+++ server.js
@@ -6,24 +6,9 @@ const fs = require("fs");
 const path = require("path");
 const staticroutes = require("./routes/staticroutes");
 const crudroutes = require("./routes/crudroutes");
-const cors = require('cors'); 
-
 
 //Creating an instance of an Express application
 const app = express();
-app.use(express.json());
-app.use(cors());
-
-
-
-// const corsOptions = {
-//   origin: 'https://notes-app-nov25-5fyx.vercel.app/' , // Replace with your actual frontend URL
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
-//   allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-// };
-
-// Handle preflight requests
-//app.options('*', cors(corsOptions));
 
 //Import static route(s)
 app.use(staticroutes);
@@ -36,5 +21,5 @@ const PORT = 3001;
 
 //Starting the server and listening on the specified port
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
